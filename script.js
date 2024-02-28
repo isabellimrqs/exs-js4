b3bc54454ab69a0309b0e9972a876b1abd9a1066
Isabelli Marques <101362239+isabellimrqs@users.noreply.github.com>
2024-02-28 07:37:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// 1) Elevado ao quadrado
+// function QuadradoDoNumero (numero) {
+//     return numero ** 2;
+// }
+
+// numeroElevado = QuadradoDoNumero(prompt('Digite um número: '));
+
+// console.log(numeroElevado)
+
+// 2) Par ou ímpar
+// function ParOuImpar(numero){
+//     if (numero % 2 === 0){
+//         alert('Número é par');
+//     } else {
+//         alert('Número é ímpar');
+//     }
+// }
+
+// numeroAdivinhar = ParOuImpar(prompt('Digite um número: ')); 
+
+// console.log(numeroAdivinhar);
